34540a71b4608b28c9bd5ba3c6a897ad4387f3cb
wangchongbei <wchb20155@gmail.com>
2017-09-20 17:32:02 +0800

    update

---
+++ http/callback.js
@@ -0,0 +1,17 @@
+function learn(something) {
+  console.log(something)
+}
+
+
+function we(callback , something){
+   something += ' is cool '
+   callback(something)
+
+}
+
+
+we(learn, 'Node JS')
+
+we(function(something){
+   console.log(something)
+},' Jade ' )
